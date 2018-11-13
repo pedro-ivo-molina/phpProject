d65a7cf708262ce0cbd1849e7746476742e7ff7e
Pedro Molina <pedro.ivo.molina@gmail.com>
2018-11-13 21:22:34 -0200

    Changed AuthorController implementation

--- controllers/AuthorController.php
+++ controllers/AuthorController.php
@@ -1,9 +1,16 @@
 <?php
-  function listAll(){
+  class AuthorController{
 
+    function __constructor(){
+
+      //code here
+
+    }
+    public function listAll(){
+
+    }
+    public function listByName($name){
+
+    }
   }
-  function listByName($name){
-
-  }
-
 ?>
